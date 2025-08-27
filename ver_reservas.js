88b4a31c212312aba309e297f1d65ba8b4c37dc6
FedericoTomasBalborin <fedebalborin@gmail.com>
2025-08-27 12:04:56 -0300

    Le agrego a la planilla de ver reservas un estado booleano así poder ver la sincronización offline/online

--- ver_reservas.js
+++ ver_reservas.js
@@ -1,7 +1,13 @@
 const db = new PouchDB('reservas');
 const remoteCouch = new PouchDB('http://admin:password@127.0.0.1:5984/reservas');
 
-db.sync(remoteCouch, { live: true, retry: true }).on('change', mostrar_reservas);
+const dbsync = db.sync(remoteCouch, { live: true, retry: true }).on('change', mostrar_reservas);
+
+async function cambiar_visto(reserva) {
+    reserva.confirmada = !reserva.confirmada;
+    await db.put(reserva);
+    mostrar_reservas();
+}
 
 async function mostrar_reservas() {
     const tabla = document.querySelector('table tbody');
@@ -16,17 +22,27 @@ async function mostrar_reservas() {
         const contacto = document.createElement('td');
         const destino = document.createElement('td');
         const fecha = document.createElement('td');
+        const estado = document.createElement('td');
         
         nombre.textContent = reserva.usuario.nombre;
         contacto.textContent = reserva.usuario.email || reserva.usuario.telefono;
         destino.textContent = `${reserva.destino.ciudad}, ${reserva.destino.pais}`;
         fecha.textContent = new Date(reserva.fecha_viaje).toLocaleDateString();
+        //El estado es un booleano que indica si la reserva está confirmada o pendiente, si clickeoo en el estado, cambia su valor
+        estado.textContent = reserva.confirmada ? 'Confirmada' : 'Pendiente';
+        estado.classList.add(reserva.confirmada ? 'confirmada' : 'pendiente');
+        estado.addEventListener('click', async () => {
+            cambiar_visto(reserva);
+        });
         tr.appendChild(nombre);
         tr.appendChild(contacto);
         tr.appendChild(destino);
         tr.appendChild(fecha);
+        tr.appendChild(estado);
         tabla.appendChild(tr);
     });
 };
 
 mostrar_reservas();
+
+dbsync.cancel()
